70eb42a7c904764a00194f4b49bc561ee570c7b1
LiamDonoghue20 <39192359+LiamDonoghue20@users.noreply.github.com>
2022-11-20 17:44:15 +0000

    end of auth section

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,12 @@
 
 using API.Data;
+using API.Interfaces;
+using API.Services;
+using System.Text;
+using API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API
 {
@@ -17,14 +23,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-
-            });
+            services.AddApplicationServices(_config);    
             services.AddControllers();
             services.AddSwaggerGen();
             services.AddCors();
-    
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +45,8 @@ namespace API
             app.UseRouting();
 
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
