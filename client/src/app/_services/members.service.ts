8f0398551cd6a25836edd418f0cd6ae5835446f1
LiamDonoghue20 <39192359+LiamDonoghue20@users.noreply.github.com>
2022-12-11 13:56:35 +0000

    end of section 10

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
@@ -10,20 +11,36 @@ export class MembersService {
   baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
+  members: Member[] = []
 
 
   getMembers(){
-    
-    return this.http.get<Member[]>(this.baseUrl + 'users')
+    //if there are currently members in the array, return it as an observable
+    if(this.members.length > 0) return of(this.members);
+    //otherwise get members from the api URL
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members
+        return members
+      })
+    )
   }
 
   getMember(username: string){
+    const member = this.members.find(x => x.userName === username);
+    if (member) return of (member);
     return this.http.get<Member>(this.baseUrl + 'users/'+username)
   
   }
 
   updateMember(member: Member) {
-   return this.http.put(this.baseUrl + 'users', member)
+   return this.http.put(this.baseUrl + 'users', member).pipe(
+    map(() => {
+      const index = this.members.indexOf(member);
+      // the ... operator takes the member thats available at that index and spreads all the info (id/username/knowas etc)
+      this.members[index] = {...this.members[index], ...member}
+    })
+   )
   }
 
 }
