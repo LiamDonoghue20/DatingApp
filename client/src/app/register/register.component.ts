e72db9050bb4ee77f53a1960aec9063161f3ee73
LiamDonoghue20 <39192359+LiamDonoghue20@users.noreply.github.com>
2022-12-13 20:32:27 +0000

    register validation

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Component, Input, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -12,27 +13,48 @@ export class RegisterComponent implements OnInit {
 
 
   @Output() cancelRegister = new EventEmitter();
-
   model: any = {}
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService,
               private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
-  register() {
-   this.accountService.register(this.model).subscribe({
-    next: () => {
-      this.cancel();
-    },
-    error: error => {
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      username: new FormControl('',Validators.required),
+      password: new FormControl('',[Validators.required, 
+                                 Validators.minLength(4), 
+                                 Validators.maxLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required, this.matchValues('password')])
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+    });
+  }
 
-      this.toastr.error(error.error),
-      console.log(error)
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+        return control.value === control.parent.get(matchTo)?.value ? null : {notMatching: true}
     }
+  }
+  register() {
+
+    console.log(this.registerForm.value);
+  //  this.accountService.register(this.model).subscribe({
+  //   next: () => {
+  //     this.cancel();
+  //   },
+  //   error: error => {
+
+  //     this.toastr.error(error.error),
+  //     console.log(error)
+  //   }
    
-   })
+  //  })
   }
 
   cancel() {
